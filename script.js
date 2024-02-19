93bf24cabfd1b3c6f08e4da43e0f7d971ffa2367
Kristian Hernandez <hernandez.cardenas10@gmail.com>
2024-02-19 13:01:00 -0600

    ci: Funcion saludar a persona

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hola Ch38")
+
+function enviarSaludo() {
+    alert("¡Hola!");
+  }
+/**
+ * Pedir el nombre de la persona usando un prompt 
+ * Mostrar un saludo y nombre de la persona con alert
+ */
+  function enviarSaludoAPersona() {
+    let name = prompt("Dime tu nombre")
+    alert("Hola " + name)
+  }
